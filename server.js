8f96b6c94a9e6123fe5b6863667225e41edf6fcf
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2015-04-14 21:26:59 +0000

    Initial API

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
+// Load Express Server
 var express = require('express');
 var app = express();
+
+
+// MongoDB Connection
+var mongoose = require('mongoose');
+var mongoOptions = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+
+mongoose.connect(process.env.DATABASE, mongoOptions);
+
+
+// Load API Router
 var apiRouter = require('./src/api/apiRouter.js');
 
 
+// Frontend
 app.use(express.static('src/frontend'));
 
+// Backend
 app.use('/admin', express.static('src/backend'));
 
+// Bower components
 app.use('/bower-components', express.static('bower_components'));
 
+// API
 app.use('/api', apiRouter);
 
 
+// Start Server
 var server = app.listen(process.env.PORT, function () {
 
   var host = server.address().address;
---
+++ src/api/api.js
@@ -0,0 +1,57 @@
+var DBCustomer = require('./model/Customer.js');
+var DBState = require('./model/State.js');
+
+var sendSuccess = function(res, message, object){
+    
+    if (object.typeof === null) {
+        object = {};
+    }
+    
+    res.json({
+        status: "success",
+        message: message,
+        object: object
+    })
+}
+
+var api = {};
+
+api.getCustomers = function(req, res) {
+    DBCustomer.find({}, function(err, customers){
+       if (err) {
+           res.send(err);
+       }
+       
+       res.json(customers);
+    });
+}
+
+api.addCustomer = function(req, res) {
+    
+}
+
+api.getStates = function(req, res) {
+    DBState.find({}, function(err, states){
+        if (err) {
+            res.send(err);
+        }
+        
+        res.json(states);
+    });
+};
+
+api.addState = function(req, res) {
+    var state = new DBState({
+        name: "Test-State"
+    });
+    
+    state.save(function(err, state){
+        if (err) {
+            res.send(err);
+        }
+        
+        sendSuccess(res, "State created", state);
+    })
+};
+
+module.exports = api;
