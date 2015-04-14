280711b13837a2fd54df2ffcd90b4cc45246eed6
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2015-04-14 11:37:10 +0000

    Added DBCategory

---
+++ api/model/DBCategory.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var categorySchema = new Schema({
+  name: String,
+  description: String
+});
+
+var Category = mongoose.model('Category', categorySchema);
+
+module.exports(Category);
