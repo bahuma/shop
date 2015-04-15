bed4d8cbe734fac0928f26c6be2806d96e6e1185
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2015-04-15 15:59:03 +0000

    Adding admin permission

--- src/api/model/User.js
+++ src/api/model/User.js
@@ -6,7 +6,8 @@ var userSchema = new Schema({
     username: String,
     password: String,
     customer: { type: Schema.Types.ObjectId, ref: 'Customer', required: false },
-    email: {type: String, required: true}
+    email: {type: String, required: true},
+    isAdmin: Boolean
 });
 
 // Validate email field
--- src/api/utilities.js
+++ src/api/utilities.js
@@ -13,4 +13,24 @@ utilities.sendSuccess = function(res, message, object){
     });
 };
 
+utilities.isAdmin = function(req, res, next) {
+    if (req.user !== undefined) {
+        if (req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).send('You must have admin rights.');
+        }
+    } else {
+        res.status(403).send('You must be logged in to do that');
+    }
+}
+
+utilities.isLoggedIn = function(req, res, next) {
+    if (req.user !== undefined) {
+        next();
+    } else {
+        res.status(403).send('You must be logged in to do that.');
+    }
+}
+
 module.exports = utilities;
