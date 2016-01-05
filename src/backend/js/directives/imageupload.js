459f51660261d4862d88ff7fb8705dcdbcc78460
Max Bachhuber <bahuma20@bahuma.io>
2016-01-05 23:10:39 +0100

    BACKEND: Prepare imageupload directive

---
+++ src/backend/js/directives/imageupload.js
@@ -0,0 +1,7 @@
+angular.module('BahumaShopBackend').directive('imageupload', function(){
+  return {
+    restrict: 'E',
+    //template: '<div class="sparkline">TEST</div>'
+    templateUrl: 'templates/directives/imageupload.html'
+  };
+});
